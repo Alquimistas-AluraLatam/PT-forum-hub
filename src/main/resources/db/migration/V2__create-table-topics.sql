f4ce372caeed84af40f1f9a93a23bfa38ddf028c
ericolivalura <eric.oliveira@alura.com.br>
2024-06-03 10:46:00 -0300

    first commit

---
+++ src/main/resources/db/migration/V2__create-table-topics.sql
@@ -0,0 +1,12 @@
+create table topics(
+
+    id bigint not null auto_increment,
+    title varchar(200) not null,
+    message varchar(2000) not null,
+    creationDate datetime not null,
+    status varchar(20) not null,
+    user_id bigint not null,
+
+    primary key(id),
+    constraint fk_topics_user_id foreign key(user_id) references users(id)
+);
